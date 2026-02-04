34bbfb926f3baccff3048eb5fb730632b34ef2ad
Jean-François Bustarret <jean-francois.bustarret@outscale.com>
2026-02-04 12:48:28 +0000

    🚸 ux(booleans): display warning when using --flag false instead of --flag=false

---
+++ cmd/prerun/false.go
@@ -0,0 +1,19 @@
+package prerun
+
+import (
+	"os"
+	"slices"
+
+	"github.com/mattn/go-isatty"
+	"github.com/outscale/gli/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+func CheckFalse(cmd *cobra.Command, args []string) {
+	if !isatty.IsTerminal(os.Stdout.Fd()) {
+		return
+	}
+	if slices.Contains(args, "false") {
+		errors.Warn("⚠️ --flag false does not work, use --flag=false")
+	}
+}
